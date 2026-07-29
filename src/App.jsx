53e4a4e08bdf3d1f0976b3e99745f5df365acc65
TiTaTo <hsb052890@gmail.com>
2026-07-29 15:30:27 +0900

    chore: test

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ function App() {
   const body = '화면이 기억할 값은 state로 관리한다.';
 
   return (
-    <main className="note-app">
-      <h1>한입 노트</h1>
-      <article className="note-card">
-        <h2>{title}</h2>
-        <p>{body}</p>
-      </article>
-    </main>
+    <>
+      <main className="note-app">
+        <h1>한입 노트</h1>
+        <article className="note-card">
+          <h2>{title}</h2>
+          <p>{body}</p>
+        </article>
+      </main>
+    </>
   );
 }
 
