19e5ed13913ffe8af6e30597df5a35897d40cfa9
TiTaTo <hsb052890@gmail.com>
2026-07-29 15:44:42 +0900

    feat: render note list

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
   const body = '화면이 기억할 값은 state로 관리한다.';
 
   return (
-    <>
       <main className="note-app">
         <h1>한입 노트</h1>
 
@@ -27,7 +26,6 @@ function App() {
         </article>
         ))}
       </main>
-    </>
   );
 }
 
